b39a2ccf527c56743a1763e29ed727739d008b45
Your Name <you@example.com>
2019-03-28 20:55:51 +0200

    New

---
+++ circulatetbooks-api/src/main/java/circulatetbooks/api/CirculatetbooksApiApplication.java
@@ -0,0 +1,13 @@
+package circulatetbooks.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CirculatetbooksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CirculatetbooksApiApplication.class, args);
+	}
+
+}
